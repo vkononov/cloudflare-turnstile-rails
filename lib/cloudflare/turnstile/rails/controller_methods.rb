7285b7e1313aee0c0538b3a149ffbd996b37315a
Vadim Kononov <vadim@konoson.com>
2025-05-19 09:27:18 -0500

    Remove unnecessary parameter.

--- lib/cloudflare/turnstile/rails/controller_methods.rb
+++ lib/cloudflare/turnstile/rails/controller_methods.rb
@@ -5,7 +5,7 @@ module Cloudflare
   module Turnstile
     module Rails
       module ControllerMethods
-        def verify_turnstile(model: nil, response: nil, **opts)
+        def verify_turnstile(model: nil, **opts)
           response ||= params[Cloudflare::RESPONSE_FIELD_NAME]
           result = Rails::Verification.verify(response: response, **opts)
 
