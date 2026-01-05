64b03d1998433dcb20fd32c3cf777b407be1158e
Vadim Kononov <vadim@konoson.com>
2026-01-04 20:10:16 -0600

    Undo unnecessary change.

--- templates/shared/test/application_system_test_case.rb
+++ templates/shared/test/application_system_test_case.rb
@@ -2,5 +2,6 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   BROWSER = ENV.fetch('BROWSER', 'chrome').to_sym
+
   driven_by :selenium, using: :"headless_#{BROWSER}", screen_size: [1400, 1400]
 end
